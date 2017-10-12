9e12942fc2891d1d496365103c9fb002c769a392
jade.yang <yunjeff@163.com>
2017-10-12 20:33:12 +0800

    main update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,44 @@
 let gulp = require('gulp'),
-  inlineCss = require('gulp-inline-css')
+    concat = require('gulp-concat'),
+    replace = require('gulp-replace'),
+    replaceTask = require('gulp-replace-task'),
+    gulpTap = require('gulp-tap'),
+    cleanCss = require('gulp-clean-css'),
+    minifyHtml = require('gulp-minify-html')
 
-gulp.task('inline-css', function() {
-  return gulp.src('./src/**/*.html')
-    .pipe(inlineCss())
-    .pipe(gulp.dest('build/'))
+let assetsMemory = ''
+/*
+  Desc: Add additional rules that need to be replaced （@17-10-12）.
+  Example: mjml default max-width=600px, And did not provide modification
+*/
+let needReplaceRules = [
+  {
+    match: 'width:600px',
+    replacement: 'width:900px'
+  }
+]
+
+gulp.task('get-css', () => {
+  return gulp.src(['./src/assets/*.css'], {read: true})
+  .pipe(cleanCss({compatibility: 'ie8'}))
+  .pipe(concat('mixing.css'))
+  .pipe(gulpTap(file => {
+    assetsMemory += file.contents.toString()
+  }))
 })
 
-gulp.task('watch', function () {
-    gulp.watch('./src/**/*.html', ['inline-css'])
+gulp.task('inject-css', ['get-css'], () => {
+  let stableCssCode = `#outlook a { padding: 0; }`
+  let needInjectCss = `${stableCssCode}\n${assetsMemory}`
+
+  return gulp.src('./src/*.html')
+    .pipe(replaceTask({
+      patterns: needReplaceRules,
+      usePrefix: false
+    }))
+    .pipe(replace(stableCssCode, needInjectCss))
+    .pipe(minifyHtml({empty: true}))
+    .pipe(gulp.dest('./build/'))
 })
 
-gulp.task('default', ['watch'])
+gulp.task('default', ['inject-css'])
